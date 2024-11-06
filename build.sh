9d1cf97afd0a63675231e61a7ed47d0839151662
vipoll <vipollmail@gmail.com>
2024-11-07 00:06:05 +0400

    add firmware package build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 # Copyright (c) 2024, Danila Tikhonov <danila@jiaxyga.com>
 
 bash ./scripts/kernel_build.sh
+bash ./scripts/firmware_build.sh
 bash ./scripts/rootfs_build.sh
---
+++ scripts/firmware_build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Copyright (c) 2024, Danila Tikhonov <danila@jiaxyga.com>
+# Copyright (c) 2024, Victor Paul <vipollmail@gmail.com>
+
+source ./scripts/vars.sh
+source ./scripts/funcs.sh
+
+set -e
+
+PKG_NAME=firmware-$VENDOR-$CODENAME
+PKG_VERSION=1.0
+FIRMWARE_DIR=${CACHE_DIR}/${PKG_NAME}
+
+# Clone the firmware repository
+mkdir -p ${CACHE_DIR}
+git_clone FIRMWARE "${CACHE_DIR}/${FOLDER_NAME}"
+
+# Clean the build directory
+rm -rf "${FIRMWARE_PACKAGE_DIR}"
+mkdir -p "${FIRMWARE_PACKAGE_DIR}"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+
+echo \
+"Name: ${PKG_NAME}
+Version: ${PKG_VERSION}
+Release: alt1
+Summary: Firmware for ${DEVICE_NAME}
+License: Distributable
+Source: %{name}-%{version}.tar.gz
+BuildArch: noarch
+
+%description
+%summary
+
+%install
+mkdir -p %{buildroot}/lib/firmware
+tar xf %SOURCE0 -C %buildroot
+
+%files
+/lib/firmware/*
+
+%changelog
+* $(LANG= date +"%a %b %d %Y") ${MAINTAINER} - 1.0-alt1
+- Initial package
+" > "${FIRMWARE_PACKAGE_DIR}"/SPECS/${PKG_NAME}.spec
+
+# Check if the files exist and copy them
+if [ -d "${FIRMWARE_DIR}/lib" ]; then
+	cp -r ${FIRMWARE_DIR}/* "${FIRMWARE_PACKAGE_DIR}"/SOURCES/
+else
+	echo "Firmware not found."
+	exit 1
+fi
+
+cd "${FIRMWARE_PACKAGE_DIR}/SOURCES"
+tar -czf "${PKG_NAME}-${PKG_VERSION}.tar.gz" *
+cd "${FIRMWARE_PACKAGE_DIR}"
+
+rpmbuild --target $ARCH --define "_topdir "${FIRMWARE_PACKAGE_DIR}"" -bb "${FIRMWARE_PACKAGE_DIR}"/SPECS/${PKG_NAME}.spec
+cp "${FIRMWARE_PACKAGE_DIR}"/RPMS/*/*.rpm "${PACKAGES_DIR}"
+
