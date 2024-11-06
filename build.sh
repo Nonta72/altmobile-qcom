41af9012890b0ca6acf24a7d4c2b7c706953a358
Danila Tikhonov <danila@jiaxyga.com>
2024-11-06 23:05:58 +0300

    scripts: Add the base rootfs_build script
    Signed-off-by: Danila Tikhonov <danila@jiaxyga.com>

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 # Copyright (c) 2024, Danila Tikhonov <danila@jiaxyga.com>
 
 bash ./scripts/kernel_build.sh
+bash ./scripts/rootfs_build.sh
---
+++ scripts/rootfs_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (c) 2024, Danila Tikhonov <danila@jiaxyga.com>
+
+source ./scripts/vars.sh
+source ./scripts/funcs.sh
+
+# Download, unpack and remove the EFI partition
+get_alt_image
