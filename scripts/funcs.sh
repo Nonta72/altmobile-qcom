4296bfc145cfe67d9a343a78440457a85ba972e7
NixXiz Lab <nixxiz@no.email>
2024-12-15 13:18:51 +0300

    fix alt mobile image downloading script

--- scripts/funcs.sh
+++ scripts/funcs.sh
@@ -57,7 +57,7 @@ remove_efi_part() {
 # Get ALT image function
 get_alt_image() {
 	local LATEST_IMAGE=$(curl -s "${ALT_URL}" | grep -oP	\
-	'alt-mobile-phosh-un-def-\d{8}-aarch64\.img\.xz'	\
+	'alt-mobile-phosh-def-\d{8}-aarch64\.img\.xz'	\
 	| sort -r | head -n 1)
 	EXTRACTED_IMAGE="${LATEST_IMAGE%.xz}"
 
