7a14deb1810edded9c3f2b73efbe3eccf0101020
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-09-01 20:18:30 +0100

    Setup environment before building

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright (c) 2024, Danila Tikhonov <danila@jiaxyga.com>
 
+bash ./scripts/buildtools.sh
 bash ./scripts/kernel_build.sh
 bash ./scripts/firmware_build.sh
 bash ./scripts/rootfs_build.sh
